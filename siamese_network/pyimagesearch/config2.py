1135f8e112ed5cb44c02e86043d5bf28878c4c88
AdarshJS <adarshjagan3895@gmail.com>
2023-06-20 18:19:16 -0400

    Deleted various unnecessary files.

--- siamese_network/pyimagesearch/config2.py
+++ siamese_network/pyimagesearch/config2.py
@@ -8,14 +8,14 @@ IMG_SHAPE = (90, 160, 3) # (height, width, channels)
 # IMG_SHAPE = (135, 240, 3) # (height, width, channels)
 
 # specify the batch size and number of epochs
-BATCH_SIZE = 64
-EPOCHS = 55 # 50 or 100
+BATCH_SIZE = 32
+EPOCHS = 75 # 50 or 100
 
 # define the path to the base output directory
-BASE_OUTPUT = "output_"+str(EPOCHS)
+BASE_OUTPUT = str(EPOCHS)
 
 # use the base output path to derive the path to the serialized
 # model along with training history plot
 # MODEL_PATH = os.path.sep.join([BASE_OUTPUT, "siamese_model"])
-MODEL_PATH = './models/' + str(EPOCHS) #'/media/asathyam/Media/spot-veg/siamese_network/output/siamese_model'
-PLOT_PATH = os.path.sep.join([BASE_OUTPUT, "plot_out55.png"])
+MODEL_PATH = './aug_demo_models/' + str(EPOCHS) #'/media/asathyam/Media/spot-veg/siamese_network/output/siamese_model'
+PLOT_PATH = os.path.sep.join([MODEL_PATH, "plot.png"])
